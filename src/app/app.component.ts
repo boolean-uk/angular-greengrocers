0799a17f176d8286d94bb6e5ef6cb9af42b1a049
Ilias Belghoul <ilias.belghoul@talan.com>
2023-08-02 13:35:02 +0200

    add core solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './models/item';
+import { GroceryService } from './grocery.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  groceries: Item[] = [];
   title = 'angular-green-grocers';
+  cart: { item: Item, quantity: number }[] = [];
+  
+  constructor(private groceryService: GroceryService) { }
+
+
+  ngOnInit() {
+    this.getGroceries();
+  }
+
+  getGroceries() {
+    this.groceryService.getGroceries().subscribe({
+      next: (data: Item[]) => {
+        this.groceries = data;
+      },
+      error: (error) => {
+        console.error('Error fetching groceries:', error);
+      }
+    });
+  }
+
+  addToCart(item: Item) {
+    const foundItem = this.cart.find(i => i.item.id === item.id);
+    if (foundItem) {
+      foundItem.quantity++;
+    } else {
+      this.cart.push({ item: item, quantity: 1 });
+    }
+  }
+  
+  decrementItem(item: Item) {
+    const foundCartItem = this.cart.find(cartItem => cartItem.item.id === item.id);
+  
+    if (foundCartItem) {
+      foundCartItem.quantity--;
+  
+      if (foundCartItem.quantity <= 0) {
+        const itemIndex = this.cart.indexOf(foundCartItem);
+        if (itemIndex > -1) {
+          this.cart.splice(itemIndex, 1);
+        }
+      }
+    }
+  
+    this.cart = [...this.cart];
+  }
+  
+  incrementItem(item: Item) {
+    const foundCartItem = this.cart.find(cartItem => cartItem.item.id === item.id);
+  
+    if (foundCartItem) {
+      foundCartItem.quantity++;
+    } else {
+      this.cart.push({item: item, quantity: 1});
+    }
+  
+    this.cart = [...this.cart];
+  }
+
+  getTotal(): string {
+    let total = 0;
+    for (let i = 0; i < this.cart.length; i++) {
+      total += this.cart[i].item.price * this.cart[i].quantity;
+    }
+    return total.toFixed(2);
+  }
+  
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { GroceryService } from './grocery.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [GroceryService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
