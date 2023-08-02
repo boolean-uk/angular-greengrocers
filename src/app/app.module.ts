4937564776d9d5734be185e3af654eb1cbafd682
Justyna Dudar <justyna.dudar@talan.com>
2023-08-02 16:07:58 +0200

    extensions done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { StoreComponent } from './store/store.component';
 import { CartComponent } from './cart/cart.component';
 import { TotalComponent } from './total/total.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, StoreComponent, CartComponent, TotalComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../models/item';
 import { GroceriesService } from '../groceries.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-store',
@@ -11,14 +12,39 @@ export class StoreComponent implements OnInit {
   constructor(private groceriesService: GroceriesService) {}
 
   items: Item[] = [];
+  itemsToShow: Item[] = [];
+  selectedFilterOption: string = 'empty';
+  selectedSortOption: string = 'empty';
 
   ngOnInit(): void {
-    this.groceriesService
-      .getAllGroceries()
-      .subscribe((groceries: Item[]) => (this.items = groceries));
+    this.groceriesService.getAllGroceries().subscribe((groceries: Item[]) => {
+      this.items = groceries;
+      this.itemsToShow = JSON.parse(JSON.stringify(this.items));
+    });
   }
 
   addToCart(item: Item) {
     this.groceriesService.addToCart(item);
+  }
+
+  onSortChange() {
+    if (this.selectedFilterOption == 'empty')
+      this.itemsToShow = JSON.parse(JSON.stringify(this.items));
+
+    if (this.selectedSortOption == 'price') {
+      this.itemsToShow.sort((item1, item2) => item1.price - item2.price);
+    } else if (this.selectedSortOption == 'name') {
+      this.itemsToShow.sort((item1, item2) =>
+        item1.name.localeCompare(item2.name)
+      );
+    }
+  }
+
+  onFilterChange() {
+    if (this.selectedFilterOption !== 'empty') {
+      this.itemsToShow = this.items.filter(
+        (item) => item.type == this.selectedFilterOption
+      );
+    } else this.itemsToShow = JSON.parse(JSON.stringify(this.items));
   }
 }
