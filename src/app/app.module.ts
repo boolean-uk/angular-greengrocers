ff65fa84faddb6f0d880a42211e41b13a9f8a1fb
Marcin Jankowski <marcin.jankowski@talan.com>
2023-08-02 12:43:35 +0200

    add item, items-list component, show items from API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ItemsListComponent, CheckoutComponent, ItemComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ItemsListComponent },
+      { path: 'checkout', component: CheckoutComponent },
+    ]),
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
