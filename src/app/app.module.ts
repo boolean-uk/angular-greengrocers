1f6696c54a2edf12b2957a5ca116553e52246f76
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-08-02 11:36:39 +0200

    getting items from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
 import { CartComponent } from './cart/cart.component';
 import { TotalComponent } from './total/total.component';
+import { StoreItemComponent } from './store-item/store-item.component';
 
 @NgModule({
-  declarations: [AppComponent, StoreComponent, CartComponent, TotalComponent],
+  declarations: [AppComponent, StoreComponent, CartComponent, TotalComponent, StoreItemComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Item } from '../models/item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  constructor(private readonly http: HttpClient) {
+  this.getAllItems().then((chars) => (this.itemsList = chars));}
+  
+  private itemsList: Item[] = [];
+
+    async getAllItems(): Promise<Item[]> {
+    const response = await firstValueFrom(
+      this.http.get<Item[]>(`${environment.apiUrl}`)
+    );
+
+    console.log('res', response);
+
+    return response;
+  }
+
+  items: Promise<Item[]> = Promise.resolve(this.itemsList);
+}
