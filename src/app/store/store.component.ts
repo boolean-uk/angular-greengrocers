477d901cfc9b888caaa48d7c662c0a230a08951e
Jan Rusak <jrusak001@gmail.com>
2023-08-02 16:31:05 +0200

    Update store component

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { StoreService } from '../../services/store.service';
+import { Item } from '../models/item';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styleUrls: ['./store.component.css'],
 })
-export class StoreComponent {
+export class StoreComponent implements OnInit, OnDestroy {
+  constructor(private readonly storeService: StoreService) {}
 
+  items?: Item[];
+  subs: Subscription[] = [];
+
+  ngOnInit(): void {
+    const sub = this.storeService
+      .getItems()
+      .subscribe((items) => (this.items = items));
+    this.subs.push(sub);
+  }
+
+  addItemToCart(item: Item): void {
+    this.storeService.addToCart(item);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((s) => s.unsubscribe());
+  }
 }
