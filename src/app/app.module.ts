c522f83ccf89e55cfc33f1c8f951e7fd449b7682
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-08-02 11:02:04 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { CartComponent } from './cart/cart.component';
+import { TotalComponent } from './total/total.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, StoreComponent, CartComponent, TotalComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
