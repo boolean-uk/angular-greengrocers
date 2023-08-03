5eba5d15d381d6e414c6b2db5a94d8611265124d
Rafal Halama <rafau.halama@gmail.com>
2023-08-03 10:47:42 +0200

    fixed remove/ made sorting persist through filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent implements OnInit, OnDestroy {
   shoppingCart: CartItem[] = [];
   subscription: Subscription | null = null;
   filterItems: boolean = true;
+  sortedType = '';
+  nameSort: boolean = true;
+  priceSort: boolean = true;
+
   constructor(private shoppingService: ShoppingService) {}
 
   getItems(): void {
@@ -23,36 +27,21 @@ export class AppComponent implements OnInit, OnDestroy {
       .getItems()
       .subscribe((items) => (this.itemList = items));
   }
-  filterByType() {
-    this.subscription = this.shoppingService
-      .getItems()
-      .subscribe(
-        (items) =>
-          (this.itemList = items.filter((item) =>
-            this.filterItems ? item.type === 'fruit' : item.type == 'vegetable'
-          ))
-      );
-    this.filterItems = !this.filterItems;
-  }
-  sortByName() {
-    this.itemList = this.itemList.sort((a, b) => a.name.localeCompare(b.name));
-  }
-  sortByPrice() {
-    this.itemList = this.itemList.sort((a, b) =>
-      a.price.toLocaleString().localeCompare(b.price.toLocaleString())
-    );
-  }
+
   getShoppingCart() {
     return this.shoppingCart;
   }
+
   ngOnInit() {
     this.getItems();
   }
+
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
+
   addToCart(item: Item, quantity: number = 1) {
     const existingCartItem = this.shoppingCart.find(
       (cartItem) => cartItem.item === item
@@ -63,10 +52,17 @@ export class AppComponent implements OnInit, OnDestroy {
       this.shoppingCart.push({ item, quantity });
     }
   }
+
   decreaseQuantity(cartItem: CartItem): void {
     const itemToChange = this.shoppingCart.find((item) => item === cartItem);
     if (itemToChange) {
       if (itemToChange.quantity > 1) itemToChange.quantity--;
+      else {
+        const index = this.shoppingCart.indexOf(itemToChange);
+        if (index !== -1) {
+          this.shoppingCart.splice(index, 1);
+        }
+      }
     }
   }
 
@@ -83,5 +79,41 @@ export class AppComponent implements OnInit, OnDestroy {
         totalPrice + cartItem.item.price * cartItem.quantity,
       0
     );
+  }
+
+  filterByType() {
+    this.subscription = this.shoppingService.getItems().subscribe((items) => {
+      this.itemList = items.filter((item) =>
+        this.filterItems ? item.type === 'fruit' : item.type === 'vegetable'
+      );
+
+      if (this.sortedType === 'price') {
+        this.sortByPrice();
+      } else if (this.sortedType === 'name') {
+        this.sortByName();
+      }
+    });
+
+    this.filterItems = !this.filterItems;
+  }
+
+  sortByName() {
+    this.itemList = this.itemList.sort((a, b) =>
+      this.nameSort === true
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)
+    );
+    this.nameSort = !this.nameSort;
+    this.sortedType = 'name';
+  }
+
+  sortByPrice() {
+    this.itemList = this.itemList.sort((a, b) =>
+      this.priceSort === true
+        ? a.price.toLocaleString().localeCompare(b.price.toLocaleString())
+        : b.price.toLocaleString().localeCompare(a.price.toLocaleString())
+    );
+    this.priceSort = !this.priceSort;
+    this.sortedType = 'price';
   }
 }
