cbebc91f8fd09552ed84cd63528aef11f458f03e
Kinga <kinga.hernet@talan.com>
2023-08-03 12:07:01 +0200

    Completed the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-green-grocers';
+  groceries: any[] = [];
+  cart: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchGroceries();
+  }
+
+  fetchGroceries() {
+    const apiUrl = 'https://boolean-api-server.fly.dev/api/groceries';
+
+    this.http.get<any[]>(apiUrl).subscribe(
+      (data) => {
+        this.groceries = data;
+      },
+      (error) => {
+        console.error('Error fetching groceries:', error);
+      }
+    );
+  }
+
+  addToCart(item: any) {
+    const existingItem = this.cart.find((cartItem) => cartItem.id === item.id);
+
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      this.cart.push({ ...item, quantity: 1 });
+    }
+  }
+
+  decreaseQuantity(item: any) {
+    const existingItem = this.cart.find((cartItem) => cartItem.id === item.id);
+
+    if (existingItem && existingItem.quantity > 1) {
+      existingItem.quantity--;
+    }
+  }
+
+  increaseQuantity(item: any) {
+    const existingItem = this.cart.find((cartItem) => cartItem.id === item.id);
+
+    if (existingItem) {
+      existingItem.quantity++;
+    }
+  }
+
+  getTotalPrice() {
+    return this.cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule to use HttpClient
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent], 
+  imports: [BrowserModule, HttpClientModule], 
   providers: [],
   bootstrap: [AppComponent],
 })
