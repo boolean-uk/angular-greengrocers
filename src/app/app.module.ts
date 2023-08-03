7af8c3120ce0f4405f0249276a7fba034a6eb57e
Julia Girejko <julia.girejko@talan.com>
2023-08-03 11:26:44 +0200

    total cost added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { GreengrocersComponent } from './greengrocers/greengrocers.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CartComponent } from './cart/cart.component';
+import { CostComponent } from './cost/cost.component';
 
 @NgModule({
-  declarations: [AppComponent, GreengrocersComponent, CartComponent],
+  declarations: [AppComponent, GreengrocersComponent, CartComponent, CostComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/grocery.service.ts
+++ src/app/grocery.service.ts
@@ -1,7 +1,7 @@
-import { Item } from './models/item';
+import { Cart, Item } from './models/item';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,6 +12,18 @@ export class GroceryService {
   constructor(private readonly http: HttpClient) { }
 
   myCart: {[key: string]: Item[]} = {}
+  total: number = 0
+
+  private totalSubject = new BehaviorSubject<number>(this.total);
+  total$ = this.totalSubject.asObservable();
+
+  get cart(): {[key: string]: Item[]}{
+    return this.myCart
+  }
+
+  get totalAmount(): number{
+    return this.total
+  }
 
   //get groceries
   async getGrocers(): Promise<Item[]>{
@@ -27,6 +39,7 @@ export class GroceryService {
         this.myCart[id].push(item)
       } else this.myCart[id] = [item]
     }
+    this.updateTotal();
   }
 
   removeFromCart(item: Item){
@@ -38,10 +51,21 @@ export class GroceryService {
         this.myCart[id].pop()
       }
     }
+    this.updateTotal();
   }
 
-  get cart(): {[key: string]: Item[]}{
-    return this.myCart
+  private updateTotal() {
+    let totalCost = 0;
+    for (const key in this.myCart) {
+      if (this.myCart.hasOwnProperty(key)) {
+        const itemsArr = this.myCart[key];
+        for (const item of itemsArr) {
+          totalCost += item.price;
+        }
+      }
+    }
+    this.total = totalCost;
+    this.totalSubject.next(totalCost);
   }
 
 }
