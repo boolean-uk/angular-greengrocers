08fe94fa8e5ce11f23c61cef8af0483d9f9d29e1
Kornel <korneldabrowski@gmail.com>
2023-08-03 13:25:55 +0200

    core done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ShoppingBasket } from './groceries/models/basket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-green-grocers';
+
+  shoppingBasket: ShoppingBasket = { items: [], total: 0 };
+
+  updateShoppingBasket(shoppingBasket: ShoppingBasket) {
+    this.shoppingBasket = shoppingBasket;
+  }
 }
--- src/app/groceries/groceries-list/groceries-list.component.ts
+++ src/app/groceries/groceries-list/groceries-list.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { GroceriesService } from '../services/groceries.service';
+import { ShoppingBasketService } from '../services/shopping-basket.service';
+import { type BasketItem } from '../models/basket';
+import { Item } from '../models/item';
+
 @Component({
   selector: 'app-groceries-list',
   templateUrl: './groceries-list.component.html',
   styleUrls: ['./groceries-list.component.css'],
 })
 export class GroceriesListComponent {
-  constructor(private readonly groceriesService: GroceriesService) {}
+  constructor(
+    private readonly groceriesService: GroceriesService,
+    private readonly shoppingBasketService: ShoppingBasketService
+  ) {}
 
   groceries = this.groceriesService.getItems();
 
-  addToCart(item: any) {
-    console.log('Adding to cart', item);
+  addToCart(item: Item) {
+    const newItem: BasketItem = {
+      ...item,
+      quantity: 1,
+    };
+
+    this.shoppingBasketService.addToBasket(newItem);
   }
 }
