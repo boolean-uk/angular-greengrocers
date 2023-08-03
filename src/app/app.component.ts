660caa0b8da14ec26f31a0477e9a6b3689b8e0e1
Ewa Skorek <37912084+akaaritah@users.noreply.github.com>
2023-08-03 13:54:37 +0200

    Core working + extension part1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,24 @@ import { Item } from './models/item';
 })
 export class AppComponent implements OnInit {
   title = 'angular-green-grocers';
+  cart: { item: Item, quantity: number }[] = [];
   items: Item[] = [
-    { id: '001-beetroot', name: 'Beetroot', price: 1.99},
-    { id: '002-carrot', name: 'Carrot', price: 0.89},
-    { id: '003-apple', name: 'Apple', price: 2.49},
-    { id: '004-apricot', name: 'Apricot', price: 0.75},
-    { id: '005-avocado', name: 'Avocado', price: 3.99},
-    { id: '006-bananas', name: 'Bananas', price: 1.25},
-    { id: '007-bell-pepper', name: 'Bell Pepper', price: 0.65},
-    { id: '008-berry', name: 'Berry', price: 1.95},
-    { id: '009-blueberry', name: 'Blueberry', price: 1.10},
-    { id: '010-eggplant', name: 'Eggplant', price: 1.49 },
+    { id: '001-beetroot', name: 'Beetroot', price: 1.99, type: 'vegetable' },
+    { id: '002-carrot', name: 'Carrot', price: 0.89, type: 'vegetable' },
+    { id: '003-apple', name: 'Apple', price: 2.49, type: 'fruit' },
+    { id: '004-apricot', name: 'Apricot', price: 0.75, type: 'fruit' },
+    { id: '005-avocado', name: 'Avocado', price: 3.99, type: 'vegetable' },
+    { id: '006-bananas', name: 'Bananas', price: 1.25, type: 'fruit' },
+    { id: '007-bell-pepper', name: 'Bell Pepper', price: 0.65, type: 'vegetable' },
+    { id: '008-berry', name: 'Berry', price: 1.95, type: 'fruit' },
+    { id: '009-blueberry', name: 'Blueberry', price: 1.10, type: 'fruit' },
+    { id: '010-eggplant', name: 'Eggplant', price: 1.49, type: 'vegetable' },
   ];
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.fetchGroceries();
+    this.filteredItems = this.items;
   }
 
   fetchGroceries() {
@@ -33,8 +35,6 @@ export class AppComponent implements OnInit {
       this.items = items;
     });
   }
-
-  cart: { item: Item, quantity: number }[] = [];
 
   addToCart(item: Item) {
     const existingItem = this.cart.find(cartItem => cartItem.item.id === item.id);
@@ -54,5 +54,28 @@ export class AppComponent implements OnInit {
 
   calculateTotal(): number {
     return this.cart.reduce((total, cartItem) => total + cartItem.item.price * cartItem.quantity, 0);
+  }
+
+  // Extensions Filter by Type, Sort:
+  filteredItems: Item[] = [];
+
+  filterByType(type: string) {
+    if (type === 'all') {
+      this.filteredItems = this.items;
+    } else {
+      this.filteredItems = this.items.filter(item => item.type === type);
+    }
+  }
+
+  sortByPrice() {
+    this.filteredItems.sort((a, b) => a.price - b.price);
+  }
+
+  sortByPriceDescending() {
+    this.filteredItems.sort((a, b) => b.price - a.price);
+  }
+
+  sortByName() {
+    this.filteredItems.sort((a, b) => a.name.localeCompare(b.name));
   }
 }
